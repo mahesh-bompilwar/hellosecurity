dec6c2fa3fba42c317b2992cdfb1e3c06bafd50b
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-10-08 18:36:53 +0530

    added jwt seciruty

---
+++ src/main/java/com/techdenovo/hellosecurity/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.techdenovo.hellosecurity.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
---
+++ src/test/java/com/techdenovo/hellosecurity/HellosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdenovo.hellosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
